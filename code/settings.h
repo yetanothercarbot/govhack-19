d311d57ca55800c13e09c079f54f82d05ca2e953
yetanothercarbot <yetanothercarbot@gmail.com>
2019-09-08 08:39:55 +1000

    Update code to use ILI9341

--- code/settings.h
+++ code/settings.h
@@ -1,9 +1,9 @@
 #define SLIDE_NO 5
 #define REPEAT_TIME 10*1000 // How much time until slideshow restarts
 
-#define SCREEN_W 160
-#define SCREEN_H 128
+#define SCREEN_W 320
+#define SCREEN_H 240
 
 String headers[] = {"Team Name", "Team Size", "Team Location", "Average Productivity", "Project Name"};
-String data[] =    {"OSE423",    "3 people",  "Brisbane", "Relatively High", "CED423"};
+String data[] =    {"OSE423",    "3 people",  "Brisbane", "Fairly High", "CED423"};
 String footers[] = {"", "", "Queensland", "like, always", ""};
