ba3fe0998a9a796ec8807e1039c074ac4b8b6c9a
yetanothercarbot <yetanothercarbot@gmail.com>
2019-09-07 17:42:40 +1000

    Add code

---
+++ code/govhack-19.ino
@@ -0,0 +1,75 @@
+// Govhack 19
+//  A display for public spaces
+//  to show information about localities.
+
+/* User settings */
+#include "settings.h"
+
+/* Code begins */
+#include <FS.h>
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
+#define FONT_SMALL "NotoSansBold15"
+#define FONT_LARGE "NotoSansBold36"
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+#define SLIDE_LENGTH REPEAT_TIME/SLIDE_NO // How long each slide should show (ms)
+
+#define COLOUR_NO 13
+#define LIGHT_COLOUR
+int colours[] = {TFT_MAROON, TFT_PURPLE, TFT_OLIVE, TFT_BLUE, TFT_MAGENTA, TFT_LIGHTGREY, TFT_GREEN, TFT_CYAN, TFT_RED, TFT_YELLOW, TFT_ORANGE, TFT_GREENYELLOW, TFT_PINK};
+
+int lastUpdate = 0;
+int counter = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  tft.init();
+  tft.setRotation(3);
+  randomSeed(analogRead(A0));
+
+  if (!SPIFFS.begin()) {
+    raiseError();
+  }
+
+  bool font_missing = false;
+  if (SPIFFS.exists("/NotoSansBold15.vlw") == false) font_missing = true;
+  if (SPIFFS.exists("/NotoSansBold36.vlw") == false) font_missing = true;
+
+  if (font_missing) {
+    Serial.println("\r\nFont missing in SPIFFS, did you upload it?");
+    while (1) yield();
+  }
+}
+
+void loop() {
+  if (millis() - lastUpdate > REPEAT_TIME / SLIDE_NO || lastUpdate == 0) {
+    updateText(TFT_BLACK, colours[random(0, COLOUR_NO)], counter);
+    counter++;
+    if (counter > SLIDE_NO) {
+      counter = 0;
+    }
+    lastUpdate = millis();
+  }
+}
+
+void updateText(int text_colour, int background_colour, int text) {
+  tft.fillScreen(background_colour);
+  tft.setTextColor(text_colour, background_colour);
+
+  tft.loadFont(FONT_SMALL); // Small font for header and footer
+  tft.drawCentreString(headers[text], SCREEN_W / 2, 0, 8);
+  tft.drawCentreString(footers[text], SCREEN_W / 2, SCREEN_H - 8, 8);
+
+  tft.loadFont(FONT_LARGE); // Small font for header and footer
+  tft.drawCentreString(data[text], SCREEN_W / 2, SCREEN_H / 2, 20);
+}
+
+void raiseError() {
+  tft.setCursor(0, 0);
+  tft.fillScreen(TFT_BLACK);
+  tft.println("Internal error!");
+  while (1) yield(); // Stay here twiddling thumbs waiting
+}
---
+++ code/settings.h
@@ -0,0 +1,9 @@
+#define SLIDE_NO 3
+#define REPEAT_TIME 10*1000 // How much time until slideshow restarts
+
+#define SCREEN_W 160
+#define SCREEN_H 128
+
+String headers[] = {"Team Name", "Team Size", "Team Location", "Average Productivity"};
+String data[] =    {"OSE423",    "3 people",  "Brisbane", "Low"};
+String footers[] = {"", "", "Queensland", "like, always"};
